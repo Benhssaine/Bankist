62f7d8f085856feb23134ac9dca24c7aa0b49464
Cataclise <94554438+Cataclise@users.noreply.github.com>
2023-04-15 23:55:58 +0000

    V1

--- coding.js
+++ coding.js
@@ -4,7 +4,7 @@ const julia = [3, 5, 2, 12, 7];
 
 const kate = [4, 1, 15, 8, 3];
 
-const checkDogs = (function (juliaArr, kateArr) {
+const checkDogs = function (juliaArr, kateArr) {
   let correctedArray = juliaArr.slice(1, -2).concat(kateArr);
   correctedArray.forEach(function (dog, index) {
     dog >= 3
@@ -13,4 +13,13 @@ const checkDogs = (function (juliaArr, kateArr) {
         )
       : console.log(`Dog number ${index + 1} is still a puppy`);
   });
-})([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+}; //([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+const calcAverageHumanAge = (function (dogs) {
+  const dogHumanAge = dogs
+    .map(value => (value <= 2 ? 2 * value : 16 + value * 4))
+    .filter(value => value >= 18)
+    .reduce((acc, value, i, array) => acc + value / array.length, 0);
+
+  console.log(dogHumanAge);
+})([11, 11, 11, 11, 11]); // => outputs 60
--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 'strict';
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Soufiane Benhssain',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
@@ -59,26 +59,7 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = (function (movement) {
-  //removes existing html in the container movements
-  containerMovements.innerHTML = '';
-  //loops the data table and creates new html which is then passed to the containermovement after begin
-  movement.forEach(function (mov, index) {
-    const movementType = mov > 0 ? 'deposit' : 'withdrawal';
-    const html = `
-    <div class="movements__row">
-      <div class="movements__type movements__type--${movementType}">${
-      index + 1
-    } ${movementType}</div>
-      <div class="movements__value">${mov}</div>
-    </div>
-  `;
-    //this method takes the placement in which the html will be placed and the html as a string
-    containerMovements.insertAdjacentHTML('afterbegin', html);
-  });
-})(account1.movements);
-
-const createUsernames = function (accs) {
+const createUsernames = (function (accs) {
   accs.forEach(account => {
     account.username = account.owner
       .toLowerCase()
@@ -86,10 +67,144 @@ const createUsernames = function (accs) {
       .map(str => str[0])
       .join('');
   });
+})(accounts);
+
+const displayMovements = function (movement) {
+  //removes existing html in the container movements
+  containerMovements.innerHTML = '';
+  //loops the data table and creates new html which is then passed to the containermovement after begin
+  movement.forEach(function (mov, index) {
+    const movementType = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${movementType}">Operation ${
+      index + 1
+    } ${movementType}</div>
+      <div class="movements__value">${mov}€</div>
+    </div>
+  `;
+    //this method takes the placement in which the html will be placed and the html as a string
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
 };
 
-createUsernames(accounts);
-console.log(accounts);
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, value) => (acc += value), 0);
+  labelBalance.textContent = `${acc.balance}€`;
+};
+
+const calcDisplaySummary = function (acc) {
+  const balanceIn = acc.movements
+    .filter(val => val >= 0)
+    .reduce((acc, val) => acc + val, 0);
+  labelSumIn.textContent = `${balanceIn}€`;
+
+  const balanceOut = acc.movements
+    .filter(val => val < 0)
+    .reduce((acc, val) => acc + val, 0);
+  labelSumOut.textContent = `${Math.abs(balanceOut)}€`;
+
+  const interest = acc.movements
+    .filter(val => val > 0)
+    .map(val => (val * acc.interestRate) / 100)
+    .filter(val => val >= 1)
+    .reduce((acc, val) => acc + val, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+
+const updateUI = function (acc) {
+  //display movements
+  displayMovements(acc.movements);
+  //display balance
+  calcDisplayBalance(acc);
+  //display summary
+  calcDisplaySummary(acc);
+};
+
+//Event handler
+let callingAccount;
+
+btnLogin.addEventListener('click', event => {
+  //prevent form from submitting and refreshing page
+  event.preventDefault();
+  //this will return an object
+  callingAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  //the callingAccount will stay undefined if the username is uncorrect or not found so without the two we cant loging
+  if (callingAccount?.pin === Number(inputLoginPin.value)) {
+    //splits the string based on space, and we only choose the first element
+    labelWelcome.textContent = `Welcome back, ${
+      callingAccount.owner.split(' ')[0]
+    }`;
+    //this will change the opacity therefore we will be able to see the app
+    containerApp.style.opacity = 100;
+
+    //clear input fields
+    inputLoginPin.value = inputLoginUsername.value = '';
+    //loses focus on the pin after pressing enter
+    inputLoginPin.blur();
+
+    updateUI(callingAccount);
+  }
+});
+
+btnTransfer.addEventListener('click', event => {
+  event.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const recieverAccount = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+  inputTransferAmount.value = inputTransferTo.value = '';
+
+  if (
+    amount > 0 &&
+    //account existence
+    recieverAccount &&
+    callingAccount.balance >= amount &&
+    // if the username doesnt exist everything fials
+    recieverAccount?.username !== callingAccount.username
+  ) {
+    callingAccount.movements.push(-amount);
+    recieverAccount.movements.push(amount);
+    updateUI(callingAccount);
+  }
+});
+
+btnLoan.addEventListener('click', event => {
+  event.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (
+    amount > 0 &&
+    callingAccount.movements.some(value => value >= amount * 0.1)
+  ) {
+    callingAccount.movements.push(amount);
+    updateUI(callingAccount);
+    inputLoanAmount.value = '';
+  }
+});
+
+btnClose.addEventListener('click', event => {
+  event.preventDefault();
+  const closeUsername = accounts.find(
+    acc => acc?.username === inputCloseUsername.value
+  );
+  console.log(closeUsername);
+  if (
+    closeUsername?.username === callingAccount?.username &&
+    callingAccount?.pin === Number(inputClosePin.value)
+  ) {
+    accounts.splice(
+      accounts.findIndex(
+        username => username?.username === callingAccount.username
+      ),
+      1
+    );
+    containerApp.style.opacity = 0;
+    labelWelcome.textContent = 'Log in to get started';
+    inputCloseUsername.value = inputClosePin.value = '';
+  }
+});
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -114,6 +229,57 @@ const movementsLog = movements.map((mov, index) => {
     mov > 0 ? 'deposited' : 'withdrew'
   } ${Math.abs(mov)}`;
 });
+
+// filters all the negative values, if the conditions is
+// true it returns the value.
+const deposits = movements.filter(value => value > 0);
+
+const withdrawals = movements.filter(value => value < 0);
+//console.log(deposits, withdrawals);
+
+// accumulator -> SnowBall
+/* const balance = movements.reduce(
+  (acc, value, index, array) => (acc += value)
+  //3000 // initializing the accumulator
+); */
+
+const max = movements.reduce((acc, value) => {
+  if (acc < value) acc = value;
+  return acc;
+}, movements[0]);
+
+//console.log(max);
+
+const euroToUsd = 1.1;
+
+totalDipositsInUSD = movements
+  .filter(value => value > 0)
+  .map(value => value * euroToUsd)
+  /*.map((value, i, arr) => {
+    console.log(arr);
+    return value * euroToUsd;
+  })*/
+  .reduce((acc, value) => acc + value, 0);
+/* 
+console.log(movements); // Array(8) [ 200, 450, -400, 3000, -650, -130, 70, 1300 ]
+console.log(movements.find(value => value < 0)); // ==> outputs -400
+
+console.log(accounts);
+console.log(accounts.find(acc => acc.owner === 'Jessica Davis')); // ==> returns the object container owner JS
+
+ */
+// this tests if there was any deposit into the account => true
+console.log(movements.some(val => val > 0));
+// this tests if there was any deposit above 5k in the account => false
+console.log(movements.some(val => val > 5000));
+
+//const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+//Seperate callBack
+const deposit = mov => mov > 0;
+
+console.log(movements.filter(deposit)); // ==> Array(5) [ 200, 450, 3000, 70, 1300 ]
+console.log(movements.some(deposit)); // ==> true
+console.log(movements.every(deposit)); // ==> false
 
 //console.log(movementsLog);
 /////////////////////////////////////////////////
